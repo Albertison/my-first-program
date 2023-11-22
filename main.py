3c07cd84ca91c70c56ba8a41954f328d821a4c2d
albert <xazivalieva2@gmail.com>
2023-11-22 14:37:58 +0300

    Version 0.2

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from PyQt5.QtWidgets import QApplication, QWidget
 import sys
+
+
+class Downloader(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(300, 250, 700, 300)
+        self.setWindowTitle('скачиватель видео')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dw = Downloader()
+    dw.show()
+    sys.exit(app.exec())
