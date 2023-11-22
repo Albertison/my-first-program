e9dec57543c2fb6df537f0af09911d34dce39342
albert <xazivalieva2@gmail.com>
2023-11-22 15:31:31 +0300

    Version 0.4

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QLabel, QPushButton
 import sys
-
+import pytube
 
 class Downloader(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setGeometry(300, 250, 700, 300)
+        self.setGeometry(250, 250, 700, 150)
         self.setWindowTitle('скачиватель видео')
         self.line = QLineEdit(self)
         self.line.setGeometry(40, 50, 600, 20)
@@ -14,6 +14,14 @@ class Downloader(QWidget):
         self.text1 = QLabel(self)
         self.text1.setGeometry(40, 20, 300, 35)
         self.text1.setText('Здравствуйте! Введите в поле ниже ссылку на видео.')
+
+        self.button = QPushButton(self)
+        self.button. setGeometry(40, 80, 100, 30)
+        self.button.setText('Установить путь')
+
+        self.buttondownload = QPushButton(self)
+        self.buttondownload.setGeometry(140, 80, 100, 30)
+        self.buttondownload.setText('Скачать')
 
 
 if __name__ == '__main__':
