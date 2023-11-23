e6537f6b3b0aae3769ad220cdcf693acb2cd098b
albert <xazivalieva2@gmail.com>
2023-11-23 10:09:54 +0300

    Version 0.9

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QLabel, QPushButton, QFileDialog
+from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QLabel, QPushButton, QFileDialog, QInputDialog
 import sys
 from pytube import YouTube
 
@@ -7,7 +7,7 @@ class Downloader(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setGeometry(250, 250, 700, 170)
+        self.setGeometry(250, 250, 700, 190)
         self.setWindowTitle('скачиватель видео')
         self.line = QLineEdit(self)
         self.line.setGeometry(40, 50, 600, 20)
@@ -17,8 +17,19 @@ class Downloader(QWidget):
         self.text1.setText('Здравствуйте! Введите в поле ниже ссылку на видео.')
 
         self.text2 = QLabel(self)
-        self.text2.setGeometry(40, 120, 300, 35)
+        self.text2.setGeometry(80, 120, 500, 35)
         self.text2.setText('')
+
+        self.text3 = QLabel(self)
+        self.text3.setGeometry(40, 120, 150, 35)
+        self.text3.setText('Путь:')
+
+        self.text4 = QLabel(self)
+        self.text4.setGeometry(40, 140, 150, 35)
+        self.text4.setText('Формат:')
+
+        self.text5 = QLabel(self)
+        self.text5.setGeometry(90, 140, 300, 35)
 
         self.button = QPushButton(self)
         self.button.setGeometry(40, 80, 100, 30)
@@ -34,10 +45,16 @@ class Downloader(QWidget):
     def work(self):
         wb_patch = QFileDialog.getExistingDirectory(self)
         self.text2.setText(wb_patch)
+        country, ok_pressed = QInputDialog.getItem(
+            self, "формат", "какой формат нужен?",
+            ("mp3", "mp4"), 1, False)
+        if ok_pressed and country:
+            self.text5.setText(country)
 
     def work2(self):
         self.myStream = YouTube(self.line.text()).streams.first()
         self.myStream.download(str(self.text2.text()))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
